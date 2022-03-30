bacee4c0477f92f370a7189c235f5bdc98e09093
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-03-30 07:47:01 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,33 +1,51 @@
 #include "main.h"
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number
- * otherwise return 0
- * @n: input number
- *
- * Return: int
+ * is_prime_number - determine if a number is a prime number
+ * @n: int number
+ * Return: 1 if prime, 0 otherwise
  */
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	if (n < 2)
 		return (0);
-	return (getNumPrim(n, 2));
+	if (n < 4)
+		return (1);
+	return (hai(n, 2));
 }
 
 /**
- * getNumPrim - function that gets if num is 0 or 1
- *
- * @num: input number
- * @i: Counter variable
- * Return: int
+ * _sqrt - return square root of number
+ * @x: number
+ * @i: number incrementer acting as divisor
+ * Return: square root of `x`
  */
 
-int getNumPrim(int num, int i)
+int _sqrt(int x, int i)
 {
-	if (num == i)
-		return (1);
-	if (num % i == 0)
+	int square;
+
+	square = i * i;
+	if (square >= x)
+		return (i);
+	else
+		return (_sqrt(x, i + 1));
+}
+
+/**
+ * hai - helper function, recursive steps taken
+ * @n: number given to original function is_prime_number
+ * @d: incrementer divisor
+ * Return: 0 if not prime, 1 if prime
+ */
+
+int hai(int n, int d)
+{
+	if (n % d == 0)
 		return (0);
-	return (getNumPrim(num, i + 1));
+	else if (_sqrt(n, 1) < d)
+		return (1);
+	else
+		return (hai(n, d + 1));
 }
