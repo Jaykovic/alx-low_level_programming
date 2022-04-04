ef14c3948f300e9c3b984b31ba9a4150695b80bd
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-04-04 08:33:03 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,7 +5,4 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-int word_count(char *str)
-char **strtow(char *str)
-int *find_words_len(char *str, int words)
 char **strtow(char *str)
