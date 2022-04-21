140917b4935fce9b57ff734808037bc0664a3f42
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-04-21 17:22:40 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,30 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-#include <string.h>
-
 /**
- * add_node - function that prints all the elements of a list_t list
- * @head: input header pointer
- * @str: Input string value
- * Return: address of the new element, or NULL if it failed
+ * add_node - Entry Point
+ * @head: input
+ * @str: string
+ * Return: 0
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	int i, count = 0;
+	list_t *new;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (head == NULL)
 		return (NULL);
-	new_node->len = _strlen(str);
-	new_node->str = strdup(str);
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
-}
 
-/**
- * _strlen - returns length of string
- * @s: character of string
- * Return: length of string
- */
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-int _strlen(const char *s)
-{
-	int i;
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
 
-	while (s[i] != 0)
-		i++;
-	return (i);
+	new->len = i;
+	new->str = strdup(str);
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
