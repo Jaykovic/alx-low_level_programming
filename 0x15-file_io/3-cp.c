e41cb036d18e75b8cd062a9ec1d29cd095cf0c35
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-05-02 21:05:48 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,120 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * exit98_check - checks if file_from exists and can be read
- * @x: int to check
- * @file: file that does not exist or cannot be read
- *
- * Return: 0 if successful, exits early with code 98 otherwise
- */
-
-int exit98_check(int x, char *file)
-{
-	if (x == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-		exit(98);
-	}
-
-	return (0);
-}
-
-/**
- * close_all - closes all file descriptors
- * @fd1: file directory 1
- * @fd2: file directory 2
- *
- * Return: 0 if successful
- */
-
-int close_all(int fd1, int fd2)
-{
-	int close_check1 = close(fd1);
-	int close_check2 = close(fd2);
-
-	if (close_check1 == -1 || close_check2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n",
-			close_check1 == -1 ? fd1 : fd2);
-		exit(100);
-	}
-
-	return (0);
-}
-
-/**
- * copy_file - copies the contents of a file to another file
- * @file_from: file to copy from
- * @file_to: file to copy to
- *
- * Return: 1 on success, -1 on failure
- */
-
-int copy_file(char *file_from, char *file_to)
-{
-	int fd_from, fd_to;
-	ssize_t read_actual, write_actual;
-	char *buffer[1024];
-
-	fd_from = open(file_from, O_RDONLY);
-	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	exit98_check(fd_from, file_from);
-
-	if (fd_to == -1)
-		return (-1);
-
-	while ((read_actual = read(fd_from, buffer, 1024)) > 0)
-	{
-		if (read_actual == -1)
-			exit98_check(read_actual, file_from);
-
-		write_actual = write(fd_to, buffer, read_actual);
-		if (write_actual == -1)
-		{
-			close_all(fd_from, fd_to);
-			return (-1);
-		}
-	}
-
-	exit98_check(read_actual, file_from);
-
-	close_all(fd_from, fd_to);
-	return (1);
-}
-
-/**
- * main - entry point function in C
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 if success
- */
+* main - program that copies the content of a file to another file
+* @argc: num argument
+* @argv: string argument
+* Return: 0
+*/
 
 int main(int argc, char *argv[])
 {
-	int copy_status;
-	char *file_from, *file_to;
+int file_from, file_to;
+int num1 = 1024, num2 = 0;
+char buf[1024];
 
-	if (argc != 3)
+if (argc != 3)
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+file_from = open(argv[1], O_RDONLY);
+if (file_from == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+}
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
+if (file_to == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	close(file_from), exit(99);
+}
+while (num1 == 1024)
+{
+	num1 = read(file_from, buf, 1024);
+	if (num1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
+	num2 = write(file_to, buf, num1);
+	if (num2 < num1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
 
-	file_from = argv[1];
-	file_to = argv[2];
+if (close(file_from) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 
-	if (file_from == NULL)
-		exit98_check(-1, file_from);
+if (close(file_to) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
-	copy_status = copy_file(file_from, file_to);
-
-	if (copy_status == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-	return (0);
+return (0);
 }
