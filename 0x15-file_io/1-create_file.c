2baf699ae2d4a2ec35b167094a8b3cdbe959c08c
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-05-02 21:03:57 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,55 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _strlen - finds length of null-terminated string
- * @s: string
- *
- * Return: length of string
- */
-
-size_t _strlen(char *s)
-{
-	size_t i = 0;
-
-	while (s[i] != '\0')
-		i++;
-
-	return (i);
-}
-
-/**
- * create_file - creates a file
- * @filename: name of file to create
- * @text_content: text to add to file as contents
- *
+ * create_file - function that creates a file.
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: Create a function that creates a file.
  * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, close_check;
-	size_t length;
-	ssize_t write_actual;
+	int i = 0, file;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-
 	if (text_content == NULL)
-		return (1);
+		text_content = "";
 
-	length = _strlen(text_content);
 
-	write_actual = write(fd, text_content, length);
-	if (write_actual == -1)
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
 		return (-1);
 
-	close_check = close(fd);
-	if (close_check == -1)
-		return (-1);
+	write(file, text_content, i);
 
 	return (1);
 }
