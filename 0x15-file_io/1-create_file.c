2fe43590b604f9ac5c6cef7a12325e35608eb791
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-05-02 20:52:06 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,55 @@
 #include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
-  * create_file - Create a file with a specified name and content
-  * @filename: Name of the file to create
-  * @text_content: Content to read into the file
-  *
-  * Return: 1 on success, -1 on any failure
-  */
+ * _strlen - finds length of null-terminated string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+size_t _strlen(char *s)
+{
+	size_t i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: text to add to file as contents
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
 int create_file(const char *filename, char *text_content)
 {
-	int file, len, closed;
-	mode_t mode = S_IRUSR | S_IWUSR;
+	int fd, close_check;
+	size_t length;
+	ssize_t write_actual;
 
-	len = 0;
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
-	if (file == -1)
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
-	{
-		while (text_content[len] != '\0')
-			len++;
-		write(file, text_content, len);
-	}
-	closed = close(file);
-	if (closed == -1)
+
+	if (text_content == NULL)
+		return (1);
+
+	length = _strlen(text_content);
+
+	write_actual = write(fd, text_content, length);
+	if (write_actual == -1)
 		return (-1);
+
+	close_check = close(fd);
+	if (close_check == -1)
+		return (-1);
+
 	return (1);
 }
